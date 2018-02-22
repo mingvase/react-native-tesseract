b0d4bfad3db28469f5ed1af724124ab701a8d11e
Ray Deck <ray@raydeck.com>
2018-02-22 15:48:40 -0500

    Bridge module created with the magic of react-native-swift-bridge!

--- ios/rn-swift-bridge.m
+++ ios/rn-swift-bridge.m
@@ -1,4 +1,8 @@
 #import <React/RCTBridgeModule.h>
 @interface RCT_EXTERN_MODULE(RNTesseract, NSObject)
+RCT_EXTERN_METHOD(setLanguage:(NSString *)newLanguage resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(setDataPath:(NSString *)newPath resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(setFastMode:(BOOL)newFast resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(recognizeCachedImage:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
 RCT_EXTERN_METHOD(recognizeFile:(NSString *)path resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
 @end
